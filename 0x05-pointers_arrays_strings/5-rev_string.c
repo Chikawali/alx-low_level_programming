cdd413efb5e6951704d0f1be5658733586b48b63
Chikawali <chykoblanko@gmailcom>
2023-02-27 05:50:30 -0800

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,6 +8,7 @@
 
 void rev_string(char *s)
 {
+
 	int len = 0, index = 0;
 	char tmp;
 
@@ -18,6 +19,6 @@ void rev_string(char *s)
 	{
 		tmp = s[index];
 		s[index] = s[len - index - 1];
-		s[len - index - 1] = temp;
+		s[len - index - 1] = tmp;
 	}
 }
