af60d69ccd74214c9e8c6e40213308f9c5613f06
Chikawali <chykoblanko@gmailcom>
2023-03-10 15:45:54 -0800

    3-islower.c

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -1,10 +1,17 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
+/**
+ * _islower - Entry point
+ *
+ * @c: character to compare
+ *
+ * Return: 1 if true, 0 if false
+ */
 
 int _islower(int c)
 {
-    return (c >= 'a' && c <= 'z');
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	return (0);
 }
