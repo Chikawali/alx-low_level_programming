2effbc076873be5f5c9272677305d78ff67d7987
Chikawali <chykoblanko@gmail.com>
2023-04-10 12:50:49 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,15 +2,16 @@
 #include <stddef.h>
 
 /**
- * read_textfile - it reads a text file and printsit to the POSIX stdio
+ * read_textfile - it reads a text file and prints it to the POSIX stdio
  * @filename: name of the file
  * @letters: it is the number of letters it should read and print
  *
- * Return: It returns the actual number of letters and prints it
- * If it cannot  be opened , it should return 0
- * If the filename is NULL, return 0
- * If write fails or does not write the expected amount of bytes, return 0
+ * Return: it returns the actual number of letters and prints it
+ * if it cannot be opened, it should return 0
+ * if the filename is NULL, return 0
+ * if write fails or does not write the expected amount of bytes, return 0
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int file, n_read, wrote;
@@ -22,16 +23,16 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		free(buffer);
 		return (0);
 	}
-	file = open(filename, O_RDONLY);
+	file = open(filename, 0_RDONLY);
 	if (file == -1)
-	return (0);
+		return (0);
 	n_read = read(file, buffer, letters);
 	if (n_read == -1)
-	return (0);
+		return (0);
 	buffer[n_read] = '\0';
 	wrote = write(STDOUT_FILENO, buffer, n_read);
 	if (wrote != n_read)
-	return (0);
+		return (0);
 	free(buffer);
 	close(file);
 	return (n_read);
